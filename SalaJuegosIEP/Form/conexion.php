0cede770667d26f74faccd29532ba79e1f32e059
sasehara2018 <sasehara0@gmail.com>
2020-07-07 09:32:52 -0300

    profe1

---
+++ SalaJuegosIEP/Form/conexion.php
@@ -0,0 +1,10 @@
+<?php
+//Open a new connection to the MySQL server
+$mysqli = new mysqli('localhost','root','root','sala_juegos');
+
+//Output any connection error
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+?>
