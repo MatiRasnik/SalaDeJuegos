12355817f476d91eb8bdb39f086a046dbea7cf22
ElTomy <tomassosa1303@gmail.com>
2020-07-15 11:46:35 -0300

    ss

--- register/logearse.php
+++ register/logearse.php
@@ -1,17 +1,13 @@
 <?php
 include '../conexion.php';
 if(isset($_POST['usuario']) && $_POST['contrasenia']){
-    $sql = "SELECT usuario FROM usuarios WHERE usuario = ".$_POST['usuario'];
-        if ($result = $mysqli -> query($sql)) {
-            $sql2 = "SELECT contra FROM usuarios WHERE contra = ".$_POST['contrasenia'];
-        if ($result = $mysqli -> query($sql2)) {
+    $sql = "SELECT contra FROM usuarios WHERE contra = ".$_POST['contrasenia'];
+    $result = $mysqli -> query($sql);
+        if ($result) {
             session_start();
             $_SESSION['usuario']=$_POST['usuario'];
-            header('Location: ../index.html');
-    }
-}else{
+        }else{
     echo "El Usuario o contaseña no son correctas";
-
-}
+    }
 }
 ?>
