bb70051a0997d9c47b034f2917bb744c5edf5cd6
Firpito <64078701+Firpito@users.noreply.github.com>
2020-07-15 12:26:57 -0300

    mandale cumbia

--- register/logearse.php
+++ register/logearse.php
@@ -1,15 +1,16 @@
 <?php
 include '../conexion.php';
 if(isset($_POST['usuario']) && $_POST['contrasenia']){
-    $sql = "SELECT contra FROM usuarios WHERE usuario = '".$_POST['usuario']."'";
+        $sql = "SELECT contra FROM usuarios WHERE usuarios = ".$_POST['contrasenia'];
     $result = $mysqli -> query($sql);
-    
         if ($result) {
-            session_start();
-            $_SESSION['usuario']=$_POST['usuario'];
-            header('Location: ../index.html');
+                if($result==$_POST['contrasenia']){
+                        session_start();
+                        $_SESSION['usuario']=$_POST['usuario'];
+                        header('Location: ../index.html');
+               }
         }else{
-        echo '<script language="javascript">alert("El usuario o la contra esta mal!");</script>';
-    }
+                echo '<script language="javascript">alert("El usuario o la contra esta mal!");</script>';
+        }
 }
 ?>
